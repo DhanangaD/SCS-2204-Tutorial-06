839992dad52ac5a5b8d7c6d455188c9db3ff4d6b
DhanangaD <88631287+DhanangaD@users.noreply.github.com>
2022-09-06 07:53:04 +0530

    Final Commit

--- Question01.scala
+++ Question01.scala
@@ -1,22 +1,27 @@
 object Main {
-    
-    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !,"
+    val KEY = 4
 
-    def Encrypt(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) + key) % a.size)
-    def Decrypt(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) - key) % a.size)
+    def shift(direction: Int, index: Int, key: Int, asize: Int) = {
+        var temp = (index + direction * key) % asize
+        if(temp < 0) temp = temp + asize
+        temp 
+    }
+    
+    def Encrypt(c: Char, key: Int, a: String) = a(shift(1, a.indexOf(c.toUpper), 4, a.size))
+
+    def Decrypt(c: Char, key: Int, a: String) = a(shift(-1, a.indexOf(c.toUpper), 4, a.size))
 
     def cipher(algo: (Char, Int, String) => Char, s: String, key: Int, a: String) = s.map(algo(_, key, a))
 
     def main(args: Array[String]) = {
+        val text = "Hello, World!"
 
-        val ct1 = cipher(Encrypt, "dhananga", 1, alphabet)
-        println(ct1)
-        val pt1 = cipher(Decrypt, ct1, 1, alphabet)
-        println(pt1)
+        val ct = cipher(Encrypt, text, KEY, ALPHABET)
+        println("\nCIPER TEXT : " + ct + "\n")
 
-        val ct2 = cipher(Encrypt, "DeEpanJaNA", 5, alphabet)
-        println(ct2)
-        val pt2 = cipher(Decrypt, ct2, 5, alphabet)
-        println(pt2) 
+        val pt = cipher(Decrypt, ct, KEY, ALPHABET)
+        println("ORIJINAL TEXT : " + pt)
+        
     }
 }
